8995b01f38c9cd8cccf170aae7fa0e31bea714cf
Diana Agafonova <diana.agafonova@nixsolutions.com>
2017-11-07 19:02:09 +0200

    create migrations

--- database/migrations/2017_11_07_163831_create_history_table.php
+++ database/migrations/2017_11_07_163831_create_history_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateHistoryTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('history', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('phone');
-            $table->rememberToken();
+            $table->integer('id_giving');
+            $table->integer('id_taker');
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->boolean('return'); // 0 if not return
             $table->timestamps();
         });
     }
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('history');
     }
 }
