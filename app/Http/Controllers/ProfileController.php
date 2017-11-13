42d3628e6f562da994ec6855b85f66a0f96ae918
Diana Agafonova <diana.agafonova@nixsolutions.com>
2017-11-13 11:00:58 +0200

    Add list of books for auth user

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\DB;
 
 class ProfileController extends Controller
 {
     public function user_profile()
     {
-        return view('profile', array('user_info' => Auth::user()));
+        $user_books = DB::table('lib_books')
+            ->join('genres', 'genres.id', '=', 'lib_books.genre_id')
+            ->join('authors_books', 'authors_books.book_id', '=', 'lib_books.id')
+            ->join('authors', 'authors.id', '=', 'authors_books.author_id')
+            ->join('user_books', 'lib_books.id', '=', 'user_books.book_id')
+            ->select('lib_books.*', 'genres.name as genre', 'authors.name as author', 'user_books.user_id as user')
+            ->where('user_books.user_id', Auth::user()->id)
+            ->get();
+        return view('profile', array('user_info' => Auth::user(), 'user_books' => $user_books));
     }
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,34 +4,68 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <h1>Profile</h1>
+            <div class="panel panel-default">
+                <div class="panel-heading">Profile</div>
 
-            <table class="table">
-                <tbody>
-                <tr>
-                    <th>Username</th>
-                    <td>{{ $user_info['username'] }}</td>
-                </tr>
-                <tr>
-                    <th>Name</th>
-                    <td>{{ $user_info['name'] }}</td>
-                </tr>
-                <tr>
-                    <th>Surname</th>
-                    <td>{{ $user_info['surname'] }}</td>
-                </tr>
-                <tr>
-                    <th>E-mail</th>
-                    <td>{{ $user_info['email'] }}</td>
-                </tr>
-                <tr>
-                    <th>Phone</th>
-                    <td>{{ $user_info['phone'] }}</td>
-                </tr>
-                </tbody>
-            </table>
+                <div class="panel-body">
+                    {{--@if (session('status'))--}}
+                        <table class="table">
+                            <tbody>
+                            <tr>
+                                <th>Username</th>
+                                <td>{{ $user_info['username'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>Name</th>
+                                <td>{{ $user_info['name'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>Surname</th>
+                                <td>{{ $user_info['surname'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>E-mail</th>
+                                <td>{{ $user_info['email'] }}</td>
+                            </tr>
+                            <tr>
+                                <th>Phone</th>
+                                <td>{{ $user_info['phone'] }}</td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    {{--@endif--}}
+                </div>
+            </div>
 
 
+            <div class="panel panel-default">
+                <div class="panel-heading">Books</div>
+
+                <div class="panel-body">
+                    <table class="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">Book name</th>
+                            <th scope="col">Author</th>
+                            <th scope="col">Year</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Genre</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($user_books as $val)
+                            <tr>
+                                <td>{{ $val->name }}</td>
+                                <td>{{ $val->author }}</td>
+                                <td>{{ $val->year }}</td>
+                                <td>{{ $val->description }}</td>
+                                <td>{{ $val->genre }}</td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
