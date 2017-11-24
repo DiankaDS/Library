14f0650a9b65c8861a03081f0a6bd15858b20f92
Diana Agafonova <diana.agafonova@nixsolutions.com>
2017-11-24 16:44:08 +0200

    Create tests

--- tests/Unit/BooksTest.php
+++ tests/Unit/BooksTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Unit;
 
+use App\Author;
 use Tests\TestCase;
+use App\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-//use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class BooksTest extends TestCase
 {
@@ -13,39 +15,154 @@ class BooksTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+//    public function testExample()
+//    {
+//        $this->assertTrue(true);
+//    }
+
+//    --- Home tests ---
+
+    public function testHomeRoutes()
     {
+//        $response = $this->call('GET', '/');
+        $response = $this->get('/');
+        $this
+            ->assertEquals(200, $response->status());
+        $response
+            ->assertViewIs('home');
+
+        $response = $this->get('/home');
+        $this
+            ->assertEquals(200, $response->status());
+
+        $response = $this->json('POST', 'search_books', ['str_book' => 'Some book']);
+        $response
+            ->assertStatus(200)
+            ->assertJson([]);
+    }
+
+
+//    public function testLogin()
+//    {
+//        $response = $this->get('login');
+////            ->see('Welcome')
+////            ->type('diana.agafonova@nixsolutions.com', 'email')
+////            ->type('111111', 'password')
+////            ->press('Login')
+////            ->dontSee('Welcome');
+//
+//        $this
+//            ->assertEquals(200, $response->status());
+//    }
+
+//    use WithoutMiddleware;
+
+
+
+//    --- Books tests ---
+
+    public function testBooksRoutes()
+    {
+        $user = User::first();
+        $this->actingAs($user);
+
+        $response = $this->get('add_book');
+        $this->assertEquals(200, $response->status());
+//        $response->assertViewIs('home');
+
+    }
+
+
+//    --- Profile tests ---
+
+    public function testProfileRoutes()
+    {
+        $user = User::first();
+        $this->actingAs($user);
+
         $this->assertTrue(true);
     }
 
 
-//    use DatabaseMigrations;
+//    --- Orders tests ---
 
-//    public function testBasicExample1()
-//    {
-//        $this->browse(function ($browser) {
-//            $browser->visit('/')
-//                ->see('Laravel 5');
-//        });
-//    }
-
-//    public function testAddBook()
-//    {
-//        $this->visit('/add_book/complete')
-//            ->type('Some book', 'name')
-//            ->type('Irvin Yalom', 'author')
-//            ->select('2', 'genre')
-////            ->type('', 'photo')
-//            ->press('Add_book')
-//            ->see("Book created!");
-//    }
-
-    public function testIndex()
+    public function testOrdersRoutes()
     {
-        $response = $this->call('GET', '/');
-        $this->assertEquals(200, $response->status());
+        $user = User::first();
+        $this->actingAs($user);
+
+        $this->assertTrue(true);
     }
 
 
+//    --- Admin tests ---
+
+    public function testAdminUsersRoutes()
+    {
+        $user = User::where('admin', '=', '1')->first();
+        $this->actingAs($user);
+
+        $response = $this->get('admin_users');
+        $response->assertStatus(200);
+    }
+
+    public function testAdminBooksRoutes()
+    {
+        $user = User::where('admin', '=', '1')->first();
+        $this->actingAs($user);
+
+        $response = $this->get('admin_books');
+        $response->assertStatus(200);
+    }
+
+    public function testAdminAuthorsRoutes()
+    {
+        $user = User::where('admin', '=', '1')->first();
+        $this->actingAs($user);
+
+        $response = $this->get('admin_authors');
+        $response->assertStatus(200);
+
+        $response = $this->post('admin_create_author', ['name' => 'Some test name']);
+        $this->assertEquals(302, $response->status());
+
+        $author_id = Author::where('name', '=', 'Some test name')->first()->id;
+        $response = $this->post('admin_del_author/' . $author_id, ['admins_author_id' => $author_id]);
+        $this->assertEquals(302, $response->status());
+    }
+
+    public function testAdminGenresRoutes()
+    {
+        $user = User::where('admin', '=', '1')->first();
+        $this->actingAs($user);
+
+        $response = $this->get('admin_genres');
+        $response->assertStatus(200);
+
+        $response = $this->post('admin_create_genre', ['genre' => 'Some test name']);
+        $this->assertEquals(302, $response->status());
+
+        $genre_id = DB::table('genres')->where('genres.name', '=', 'Some test name')->first()->id;
+        $response = $this->post('admin_del_genre/' . $genre_id, ['admins_genre_id' => $genre_id]);
+        $this->assertEquals(302, $response->status());
+    }
+
+    public function testAdminOrdersRoutes()
+    {
+        $user = User::where('admin', '=', '1')->first();
+        $this->actingAs($user);
+
+        $response = $this->get('admin_orders');
+        $response->assertStatus(200);
+    }
+
+    public function testAdminReviewsRoutes()
+    {
+        $user = User::where('admin', '=', '1')->first();
+        $this->actingAs($user);
+
+        $response = $this->get('admin_reviews');
+        $response->assertStatus(200);
+    }
 
 }
