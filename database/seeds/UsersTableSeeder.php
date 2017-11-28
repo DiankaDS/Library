41338d80e316162512a963ba08076ccde179be55
Diana Agafonova <diana.agafonova@nixsolutions.com>
2017-11-28 12:55:17 +0200

    Create seeds

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+//use Illuminate\Support\Facades\DB;
+use App\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for($i=1; $i<10; $i++) {
+            User::insert([
+                'username' => str_random(10),
+                'name' => str_random(10),
+                'surname' => str_random(10),
+                'email' => str_random(10) . '@user.com',
+                'phone' => mt_rand(1000, 999999),
+                'password' => bcrypt('user'),
+                'photo' => 'default_user.jpg',
+                'created_at' => Now(),
+                'updated_at' => Now(),
+            ]);
+        }
+    }
+}
