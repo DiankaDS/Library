132a4f0ac7fb8cb4d7e0de6df356bd0a0e8a532a
Diana Agafonova <diana.agafonova@nixsolutions.com>
2017-12-20 12:20:34 +0200

    Hidden old functions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,30 +11,30 @@ use App\Author;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-        $books = DB::table('lib_books')
-            ->join('authors_books', 'authors_books.book_id', '=', 'lib_books.id')
-            ->join('authors', 'authors.id', '=', 'authors_books.author_id')
-            ->select('lib_books.*', DB::raw('group_concat(authors.name) as author'), DB::raw("(
-                SELECT sum(reviews.rating) 
-                FROM reviews
-                WHERE reviews.book_id = lib_books.id
-                ) as rating"))
-            ->whereIn('lib_books.id',function($query) {
-                $query->select('book_id')->from('user_books');
-            })
-            ->groupBy('lib_books.id')
-            ->orderBy('rating', 'DESC')
-            ->paginate(12);
-
-        $genres = DB::table('genres')->get();
-
-        return view('home', [
-            'books' => $books,
-            'genres' => $genres,
-        ]);
-    }
+//    public function index()
+//    {
+//        $books = DB::table('lib_books')
+//            ->join('authors_books', 'authors_books.book_id', '=', 'lib_books.id')
+//            ->join('authors', 'authors.id', '=', 'authors_books.author_id')
+//            ->select('lib_books.*', DB::raw('group_concat(authors.name) as author'), DB::raw("(
+//                SELECT sum(reviews.rating)
+//                FROM reviews
+//                WHERE reviews.book_id = lib_books.id
+//                ) as rating"))
+//            ->whereIn('lib_books.id',function($query) {
+//                $query->select('book_id')->from('user_books');
+//            })
+//            ->groupBy('lib_books.id')
+//            ->orderBy('rating', 'DESC')
+//            ->paginate(12);
+//
+//        $genres = DB::table('genres')->get();
+//
+//        return view('home', [
+//            'books' => $books,
+//            'genres' => $genres,
+//        ]);
+//    }
 
 
     public function homeSearch()
--- public/js/main.js
+++ public/js/main.js
@@ -48,9 +48,7 @@ function newCheckTip(e, id){
         }
     }, 1000);
 }
-
-//=============================================================================================================================================
-
+//======================================================================================================================
 // Show years
 $(document).ready(function(){
     var list = $("#years_list_unchecked a");
@@ -92,10 +90,8 @@ $(document).ready(function(){
         }
     });
 });
-
-//=============================================================================================================================================
+//======================================================================================================================
 // Parse url
-
 window.onload = function() {
     // if (window.location.pathname == "/home_search"
     if (window.location.pathname == "/"
@@ -196,8 +192,7 @@ window.onload = function() {
         }
     }
 };
-
-//=============================================================================================================================================
+//======================================================================================================================
 function clickCheckbox(e, id) {
     if ($(e.currentTarget).prop('checked')) {
         $('#' + id + '_list_checked').append($(e.currentTarget).parent().parent());
@@ -207,7 +202,7 @@ function clickCheckbox(e, id) {
     }
     newSearchBook(event);
 }
-
+//======================================================================================================================
 function newSearchBook(e, page){
 
     var input = $('#mySearch').val();
@@ -398,8 +393,6 @@ function newSearchBook(e, page){
 //=============================================================================================================================================
 //=============================================================================================================================================
 
-
-
 // --- Confirm window ---
 function myModal(id, textBody, textTitle) {
     textTitle = "Please, confirm action!";
@@ -413,7 +406,7 @@ function myModal(id, textBody, textTitle) {
     $("#myModal .modal-title").text(textTitle);
     $("#myModal .modal-body").text(textBody);
 }
-
+//======================================================================================================================
 function addTagModal(id, all_tags) {
 
     $('#myModal').modal('show');
@@ -477,85 +470,85 @@ function addTagModal(id, all_tags) {
         body.append(p);
     }
 }
-
+//======================================================================================================================
 // --- Ajax in home (find tags) ---
-setTimeout(function() {
-
-    $(".bootstrap-tagsinput input").keyup(function () {
-
-        // console.log($(".bootstrap-tagsinput input").val());
-
-        timer=setTimeout(function() {
-            clearTips();
-            var timer;
-            clearTimeout(timer);
-
-            var input = $(".bootstrap-tagsinput input");
-
-            if (input.val() != '') {
-
-                $.ajax({
-                    'headers': {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
-                    'type': 'post',
-                    'url': 'search_value',
-                    'data': {
-                        'str': input.val(),
-                        'id': 'tags'
-                    },
-                    success: function (data) {
-                        var source = $.parseJSON(data);
-
-                        if (source != '') {
-                            // var tips = $("<ul class='tips dropdown-menu'></ul>");
-                            var tips = $("<div class='tips'></div>");
-                            tips.css('position', 'absolute');
-                            tips.css('background', 'rgba(255,255,255,0.9)');
-                            tips.css('border', 'solid 1px #ccc');
-                            tips.css('width', '100%');
-                            tips.css('cursor', 'pointer');
-                            tips.css('z-index', '1');
-
-                            // console.log(input.val());
-                            // console.log(data);
-                            // console.log(source);
-
-                            for (var i = 0; i < source.length; i++) {
-                                // var tip = $("<div class='tip'>" + source[i]['name'] + "</div>");
-                                // var tip = $("<li class='tip'><a href='#'>" + source[i]['name'] + "</a></li>");
-                                var tip = $("<div class='tip'>" + source[i]['name'] + "</div>");
-                                tip.click(function (e) {
-                                    var x = $(e.currentTarget).parent().parent().find('span[data-role="remove"]');
-                                    x.eq(x.length-1).trigger('click');
-
-                                    $(e.currentTarget).parent().parent().find('input').val($(e.currentTarget).text());
-                                    $(e.currentTarget).parent().parent().find('input').blur();
-
-                                    clearTips();
-                                });
-                                tips.append(tip);
-                            }
-
-                            // var a = $("<a class='dropdown'></a>");
-                            // $(".bootstrap-tagsinput input").appendTo(a);
-                            // $(".bootstrap-tagsinput input").attr("data-toggle", "dropdown");
-
-                            // tips.appendTo($(".bootstrap-tagsinput input"));
-                            tips.appendTo(input.parent());
-                        }
-                    },
-                    error: function (x, e) {
-                        console.log(x);
-                        console.log(e);
-                    }
-                });
-            }
-        }, 1000);
-
-    });
-}, 1000);
-
+// setTimeout(function() {
+//
+//     $(".bootstrap-tagsinput input").keyup(function () {
+//
+//         // console.log($(".bootstrap-tagsinput input").val());
+//
+//         timer=setTimeout(function() {
+//             clearTips();
+//             var timer;
+//             clearTimeout(timer);
+//
+//             var input = $(".bootstrap-tagsinput input");
+//
+//             if (input.val() != '') {
+//
+//                 $.ajax({
+//                     'headers': {
+//                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+//                     },
+//                     'type': 'post',
+//                     'url': 'search_value',
+//                     'data': {
+//                         'str': input.val(),
+//                         'id': 'tags'
+//                     },
+//                     success: function (data) {
+//                         var source = $.parseJSON(data);
+//
+//                         if (source != '') {
+//                             // var tips = $("<ul class='tips dropdown-menu'></ul>");
+//                             var tips = $("<div class='tips'></div>");
+//                             tips.css('position', 'absolute');
+//                             tips.css('background', 'rgba(255,255,255,0.9)');
+//                             tips.css('border', 'solid 1px #ccc');
+//                             tips.css('width', '100%');
+//                             tips.css('cursor', 'pointer');
+//                             tips.css('z-index', '1');
+//
+//                             // console.log(input.val());
+//                             // console.log(data);
+//                             // console.log(source);
+//
+//                             for (var i = 0; i < source.length; i++) {
+//                                 // var tip = $("<div class='tip'>" + source[i]['name'] + "</div>");
+//                                 // var tip = $("<li class='tip'><a href='#'>" + source[i]['name'] + "</a></li>");
+//                                 var tip = $("<div class='tip'>" + source[i]['name'] + "</div>");
+//                                 tip.click(function (e) {
+//                                     var x = $(e.currentTarget).parent().parent().find('span[data-role="remove"]');
+//                                     x.eq(x.length-1).trigger('click');
+//
+//                                     $(e.currentTarget).parent().parent().find('input').val($(e.currentTarget).text());
+//                                     $(e.currentTarget).parent().parent().find('input').blur();
+//
+//                                     clearTips();
+//                                 });
+//                                 tips.append(tip);
+//                             }
+//
+//                             // var a = $("<a class='dropdown'></a>");
+//                             // $(".bootstrap-tagsinput input").appendTo(a);
+//                             // $(".bootstrap-tagsinput input").attr("data-toggle", "dropdown");
+//
+//                             // tips.appendTo($(".bootstrap-tagsinput input"));
+//                             tips.appendTo(input.parent());
+//                         }
+//                     },
+//                     error: function (x, e) {
+//                         console.log(x);
+//                         console.log(e);
+//                     }
+//                 });
+//             }
+//         }, 1000);
+//
+//     });
+// }, 1000);
+//======================================================================================================================
 // --- Ajax in add_book and home (find text in input) ---
 function checkTip(e, id){
 
@@ -612,11 +605,11 @@ function checkTip(e, id){
     }
     }, 1000);
 }
-
+//======================================================================================================================
 function clearTips(){
     $('.tips').remove();
 }
-
+//======================================================================================================================
 // --- Google search in add_book (find text in input) ---
 function googleSearch(e){
 
@@ -700,7 +693,7 @@ function googleSearch(e){
         }
     }, 1000);
 }
-
+//======================================================================================================================
 function googleModal(name, author, genre, year, description, photo) {
     $('#myModal').modal('show');
     $('#YesButton').off('click');
@@ -748,86 +741,86 @@ function googleModal(name, author, genre, year, description, photo) {
 
     body.append(p);
 }
-
+//======================================================================================================================
 // --- Ajax in Home (search books) ---
-function searchBook(){
-    var input_book = $("#mySearchBook");
-    var input_author = $("#mySearchAuthor");
-    var input_year = $("#mySearchYear");
-    var input_genre = $("#mySearchGenre");
-    var input_tags = $("#mySearchTags");
-
-
-    // console.log(input_tags.val());
-
-    $.ajax({
-        'headers': {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        },
-        'type': 'post',
-        'url': 'search_books',
-        'data': {
-            'str_book': input_book.val(),
-            'str_author': input_author.val(),
-            'str_year': input_year.val(),
-            'str_genre': input_genre.val(),
-            'arr_tags': input_tags.val()
-        },
-        success: function (data) {
-            var source = $.parseJSON(data);
-            $("#myBooks").empty();
-
-            // console.log(input_tags.val());
-            // console.log(data);
-            // console.log(source);
-
-            if (source.length !== 0) {
-                for (var i = 0; i < source.length; i++) {
-                    var container = $('<div class="col-md-3"></div>');
-                    container.appendTo($("#myBooks"));
-                    var thumb = $('<div class="thumbnail" style="width: 250px; height: 300px;"></div>');
-                    thumb.appendTo(container);
-
-                    var a = $('<a href="book_' + source[i].id + '" name="' + source[i].id + '">');
-                    a.appendTo(thumb);
-
-                    if (source[i].photo != 0) {
-                        var img = $('<img src="' + source[i].photo + '" style="width: 125px; height: 150px;">');
-                    }
-                    else{
-                        var img = $('<img src="../images/default_book.jpg" style="width: 125px; height: 150px;">');
-                    }
-                    img.appendTo(a);
-
-                    var caption = $('<div class="caption"></div>');
-                    caption.appendTo(thumb);
-
-                    var pa = $('<p align="center" style="text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;"><a href="book_' + source[i].id + '" name="' + source[i].id + '">' + source[i].name + '</a></div>');
-                    pa.appendTo(caption);
-
-                    var p = $('<p align="center" style="text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;">' + source[i].author + ', ' + source[i].year + '</p>');
-                    p.appendTo(caption);
-
-                    if (source[i].rating) {
-                        var rating = $('<p align="center">Rating: <b>' + source[i].rating + '</b></p>');
-                    }
-                    else {
-                        var rating = $('<p align="center">Rating: <b>0</b></p>');
-                    }
-                    rating.appendTo(caption);
-                }
-            }
-            else{
-                $('<p align="center">No result found...</p>').appendTo($("#myBooks"));
-            }
-        },
-        error: function (x, e) {
-            console.log(x);
-            console.log(e);
-        }
-    });
-}
-
+// function searchBook(){
+//     var input_book = $("#mySearchBook");
+//     var input_author = $("#mySearchAuthor");
+//     var input_year = $("#mySearchYear");
+//     var input_genre = $("#mySearchGenre");
+//     var input_tags = $("#mySearchTags");
+//
+//
+//     // console.log(input_tags.val());
+//
+//     $.ajax({
+//         'headers': {
+//             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+//         },
+//         'type': 'post',
+//         'url': 'search_books',
+//         'data': {
+//             'str_book': input_book.val(),
+//             'str_author': input_author.val(),
+//             'str_year': input_year.val(),
+//             'str_genre': input_genre.val(),
+//             'arr_tags': input_tags.val()
+//         },
+//         success: function (data) {
+//             var source = $.parseJSON(data);
+//             $("#myBooks").empty();
+//
+//             // console.log(input_tags.val());
+//             // console.log(data);
+//             // console.log(source);
+//
+//             if (source.length !== 0) {
+//                 for (var i = 0; i < source.length; i++) {
+//                     var container = $('<div class="col-md-3"></div>');
+//                     container.appendTo($("#myBooks"));
+//                     var thumb = $('<div class="thumbnail" style="width: 250px; height: 300px;"></div>');
+//                     thumb.appendTo(container);
+//
+//                     var a = $('<a href="book_' + source[i].id + '" name="' + source[i].id + '">');
+//                     a.appendTo(thumb);
+//
+//                     if (source[i].photo != 0) {
+//                         var img = $('<img src="' + source[i].photo + '" style="width: 125px; height: 150px;">');
+//                     }
+//                     else{
+//                         var img = $('<img src="../images/default_book.jpg" style="width: 125px; height: 150px;">');
+//                     }
+//                     img.appendTo(a);
+//
+//                     var caption = $('<div class="caption"></div>');
+//                     caption.appendTo(thumb);
+//
+//                     var pa = $('<p align="center" style="text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;"><a href="book_' + source[i].id + '" name="' + source[i].id + '">' + source[i].name + '</a></div>');
+//                     pa.appendTo(caption);
+//
+//                     var p = $('<p align="center" style="text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; overflow: hidden;">' + source[i].author + ', ' + source[i].year + '</p>');
+//                     p.appendTo(caption);
+//
+//                     if (source[i].rating) {
+//                         var rating = $('<p align="center">Rating: <b>' + source[i].rating + '</b></p>');
+//                     }
+//                     else {
+//                         var rating = $('<p align="center">Rating: <b>0</b></p>');
+//                     }
+//                     rating.appendTo(caption);
+//                 }
+//             }
+//             else{
+//                 $('<p align="center">No result found...</p>').appendTo($("#myBooks"));
+//             }
+//         },
+//         error: function (x, e) {
+//             console.log(x);
+//             console.log(e);
+//         }
+//     });
+// }
+//======================================================================================================================
 // --- Edit review in book_details ---
 function editReview(id, text) {
     var form = '<form id="edit_review_form" method="post" action="add_review">' +
@@ -881,7 +874,7 @@ function sortTable(id, n) {
         }
     }
 }
-
+//======================================================================================================================
 // --- Datepicker ---
 $(document).ready(function(){
     var date_input=$('input[id="datepicker"]'); //our date input has the name "date"
@@ -893,7 +886,7 @@ $(document).ready(function(){
         autoclose: true
     })
 });
-
+//======================================================================================================================
 // --- Add book to wished ---
 function wishBook(){
     $('#wish').val(1);
